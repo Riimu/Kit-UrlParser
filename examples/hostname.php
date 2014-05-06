499f89f35474457f1f45b248d79b1871dd5d8fbb
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2014-05-06 13:40:09 +0300

    Fix example

--- examples/hostname.php
+++ examples/hostname.php
@@ -1,7 +1,7 @@
 <?php
 
-set_include_path(__DIR__ . '/../src');
-spl_autoload_register();
+include '../src/UrlParser.php';
+include '../src/UrlInfo.php';
 
 $parser = new \Riimu\Kit\UrlParser\UrlParser();
 $info = $parser->parseUrl('http://foo:bar@www.example.com:80/path/part?query=part#fragmentPart');
