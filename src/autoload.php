d76b819ae777810721eaa5913ef0dd25c189a26f
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2017-07-05 13:42:24 +0300

    Provide a clarifying comment

--- src/autoload.php
+++ src/autoload.php
@@ -2,6 +2,7 @@
 
 namespace Riimu\Kit\UrlParser;
 
+// Bundled autoloader provided as an optional alternative to composer autoloader
 spl_autoload_register(function ($class) {
     if (strncmp($class, __NAMESPACE__, strlen(__NAMESPACE__)) === 0) {
         $path = __DIR__ . strtr(substr($class, strlen(__NAMESPACE__)), ['\\' => DIRECTORY_SEPARATOR]) . '.php';
