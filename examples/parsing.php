e35dc4348eca0ea30b656df66d3b45a363db38d5
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2017-07-15 16:01:56 +0300

    Update documentation

--- examples/parsing.php
+++ examples/parsing.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $parser = new \Riimu\Kit\UrlParser\UriParser();
 $uri = $parser->parse('http://jane:pass123@www.example.com:8080/site/index.php?action=login&prev=index#form');
--- examples/validation.php
+++ examples/validation.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $getValidUrl = function ($url) {
     $parser = new \Riimu\Kit\UrlParser\UriParser();
